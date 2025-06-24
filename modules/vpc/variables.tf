a2070155082497f12b9cfa534365affb4712fa8f
iam-raju <163100276+iam-raju@users.noreply.github.com>
2025-06-24 17:19:14 +0530

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,1 +1,11 @@
+variable "vpc_name" {
+  description = "Name to assign to VPC and its related resources"
+  type        = string
+  default     = "my-vpc"
+}
 
+variable "cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
