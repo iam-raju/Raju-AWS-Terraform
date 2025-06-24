1865f29ef5d69618ce95373b7e86635d94648d49
iam-raju <163100276+iam-raju@users.noreply.github.com>
2025-06-24 17:35:40 +0530

    Update main.tf

--- live/dev/main.tf
+++ live/dev/main.tf
@@ -1,12 +1,26 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.11.0"
-    }
-  }
+module "vpc" {
+  source = "../../modules/vpc"
+
+  cidr = "10.0.0.0/16"
 }
 
-provider "aws" {
-  region = "us-east-1"
+module "alb" {
+  source = "../../modules/alb"
+
+  lb_name           = "raju-alb"
+  vpc_id            = module.vpc.vpc_id
+  subnet_ids        = module.vpc.public_subnet_ids
+  security_group_id = module.vpc.web_sg_id
+  target_group_name = "raju-tg"
+}
+
+module "ec2" {
+  source = "../../ec2/modules/ec2_instance"
+
+  instance_name       = "raju-web"
+  ami_id              = "ami-0261755bbcb8c4a84"
+  instance_type       = "t2.micro"
+  subnet_id           = module.vpc.public_subnet_ids[0]
+  security_group_id   = module.vpc.web_sg_id
+  user_data_file_path = "${path.module}/userdata.sh"
 }
