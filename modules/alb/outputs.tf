74638ba186e120e6194dcff8b10eee60b73c7099
iam-raju <163100276+iam-raju@users.noreply.github.com>
2025-06-24 17:30:54 +0530

    Update outputs.tf

--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -1,1 +1,15 @@
+output "alb_arn" {
+  description = "ARN of the Application Load Balancer"
+  value       = aws_lb.this.arn
+}
+
+output "alb_dns_name" {
+  description = "DNS name of the Load Balancer"
+  value       = aws_lb.this.dns_name
+}
+
+output "target_group_arn" {
+  description = "ARN of the Target Group"
+  value       = aws_lb_target_group.this.arn
+}
 
