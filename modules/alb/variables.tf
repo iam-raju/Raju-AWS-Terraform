03ea4cdaec7a54273d649e92bd5fd3aaa4922e3e
iam-raju <163100276+iam-raju@users.noreply.github.com>
2025-06-24 17:30:18 +0530

    Update variables.tf

--- modules/alb/variables.tf
+++ modules/alb/variables.tf
@@ -1,1 +1,26 @@
+variable "lb_name" {
+  description = "Name of the Application Load Balancer"
+  type        = string
+  default     = "my-alb"
+}
 
+variable "subnet_ids" {
+  description = "List of public subnet IDs for ALB"
+  type        = list(string)
+}
+
+variable "vpc_id" {
+  description = "VPC ID where the ALB and target group will be created"
+  type        = string
+}
+
+variable "security_group_id" {
+  description = "Security group ID to associate with the ALB"
+  type        = string
+}
+
+variable "target_group_name" {
+  description = "Name of the target group"
+  type        = string
+  default     = "my-target-group"
+}
