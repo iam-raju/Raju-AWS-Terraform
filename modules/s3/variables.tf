ea11c602a9fe6107327001a7a07ae01b0d728da2
iam-raju <163100276+iam-raju@users.noreply.github.com>
2025-06-25 16:08:58 +0530

    Update variables.tf

--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -1,1 +1,23 @@
+variable "bucket_name" {
+  type        = string
+  description = "The name of the S3 bucket to create"
+}
+
+variable "acl" {
+  type        = string
+  default     = "private"
+  description = "Canned ACL to apply (e.g., private, public-read)"
+}
+
+variable "versioning_enabled" {
+  type        = bool
+  default     = false
+  description = "Enable versioning on the bucket"
+}
+
+variable "environment" {
+  type        = string
+  default     = "dev"
+  description = "Tag to identify environment (e.g., dev, prod)"
+}
 
