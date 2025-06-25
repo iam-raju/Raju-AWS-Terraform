2d7df496e14b0f3e4d1f8c4b9bae7d806e747111
iam-raju <163100276+iam-raju@users.noreply.github.com>
2025-06-25 16:07:57 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,1 +1,17 @@
+resource "aws_s3_bucket" "this" {
+  bucket = var.bucket_name
+  acl    = var.acl
 
+  tags = {
+    Name        = var.bucket_name
+    Environment = var.environment
+  }
+}
+
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  versioning_configuration {
+    status = var.versioning_enabled ? "Enabled" : "Suspended"
+  }
+}
