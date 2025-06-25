4e710645d9ee123cb99d8daa601cd7387a954756
iam-raju <163100276+iam-raju@users.noreply.github.com>
2025-06-25 16:09:35 +0530

    Update outputs.tf

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -1,1 +1,10 @@
+output "bucket_name" {
+  value       = aws_s3_bucket.this.bucket
+  description = "Name of the created S3 bucket"
+}
+
+output "bucket_arn" {
+  value       = aws_s3_bucket.this.arn
+  description = "ARN of the created S3 bucket"
+}
 
